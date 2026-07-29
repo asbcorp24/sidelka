31b11ac541e8141b6d520372ab1b60f57588bef9
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-29 09:20:19 +0300

    Добавить защищённый маршрут предпросмотра сканов

--- routes/documents.php
+++ routes/documents.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\ContractController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/cabinet/legal/documents/{document}/preview', [ContractController::class, 'previewDocument'])
+    ->name('contracts.document.preview');
+
 Route::get('/cabinet/legal/documents/{document}/download', [ContractController::class, 'downloadDocument'])
     ->name('contracts.document.download');
